ecb0237fd3f4eb3051ebaffdf3dda65bc01f0e03
mtmodesti <marcelomodesti1988@gmail.com>
2022-05-17 11:15:14 -0300

    fix: insertion of entities references in data source

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,6 +1,6 @@
 import { DataSource } from "typeorm";
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
@@ -14,10 +14,11 @@ export const AppDataSource = new DataSource({
   synchronize: false,
   logging: true,
   migrations: ["/src/migrations/*.ts"],
+  entities: ["/src/entities/*.ts"],
 });
 
-AppDataSource.initialize().then(() => {
-    console.log(
-      "Data Source initialized"
-  );
-}).catch(err => console.error('Error during data source initialization'));
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Data Source initialized");
+  })
+  .catch((err) => console.error("Error during data source initialization"));

adf9b6599dea45e615eb6709f2c8824c2a79fab9
Pedro <pedro@gmail.com>
2022-05-20 20:51:42 -0300

    fix:saving

--- src/routes/usuario.routes.ts
+++ src/routes/usuario.routes.ts
@@ -2,14 +2,15 @@ import { Router } from "express";
 import UsuarioController from "../controllers/Usuario.controller";
 import usuarioInfoExiste from "../middlewares/usuarioInfoExiste.middlewares";
 
-
-
-
 const usuarioRouter = Router();
 
-usuarioRouter.post("/criar-usuario", UsuarioController.store); 
-usuarioRouter.get("/listar", UsuarioController.index); 
-usuarioRouter.patch("/atualizar/:id", usuarioInfoExiste, UsuarioController.update);  
+usuarioRouter.post("/criar-usuario", UsuarioController.store);
+usuarioRouter.get("/listar", UsuarioController.index);
+usuarioRouter.patch(
+  "/atualizar/:id",
+  usuarioInfoExiste,
+  UsuarioController.update
+);
 usuarioRouter.delete("/deletar/:id", UsuarioController.delete);
 
 export default usuarioRouter;

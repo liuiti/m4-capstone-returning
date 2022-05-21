ded0156be5a013541d676f03882bdd30e66abb91
fabio <fabiofutagawa@gmail.com>
2022-05-20 21:31:18 -0300

    feat: change to deploy on heroku

--- src/data-source.ts
+++ src/data-source.ts
@@ -13,15 +13,20 @@ export const AppDataSource =
       })
     : new DataSource({
         type: "postgres",
-        host: "localhost",
-        port: 45432,
-
-        username: process.env.POSTGRES_USER,
-        database: process.env.POSTGRES_DB,
-        password: process.env.POSTGRES_PASSWORD,
+        url: process.env.DATABASE_URL,
+        ssl:
+          process.env.NODE_ENV === "production"
+            ? { rejectUnauthorized: false }
+            : false,
 
         synchronize: false,
         logging: true,
-        migrations: ["src/migrations/*.ts"],
-        entities: ["src/models/*.ts"],
+        entities:
+          process.env.NODE_ENV === "production"
+            ? ["dist/entities/*.js"]
+            : ["src/entities/*.ts"],
+        migrations:
+          process.env.NODE_ENV === "production"
+            ? ["dist/migrations/*.js"]
+            : ["src/migrations/*.ts"],
       });

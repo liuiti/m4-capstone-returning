d14a5184d90ee743848a10b1b3e54b526d237008
fabio <fabiofutagawa@gmail.com>
2022-05-25 23:48:42 -0300

    feat: user test implemented

--- src/services/usuario/deletarUsuario.service.ts
+++ src/services/usuario/deletarUsuario.service.ts
@@ -3,14 +3,10 @@ import { AppDataSource } from "../../data-source";
 import AppError from "../../errors/AppError";
 
 export default class DeletarUsuarioService {
-   async execute(id: string): Promise<Usuario> {
+  async execute(id: string): Promise<Usuario> {
     const usuarioRepositorio = AppDataSource.getRepository(Usuario);
 
-    console.log("cheguei no controller");
-
     const usuario = await usuarioRepositorio.findOne({ where: { id } });
-
-    console.log("não cheguei aqui");
 
     if (!usuario) {
       throw new AppError("Não foi encontrado nenhum usuario com esse id", 404);
